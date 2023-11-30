c0b540f036dc866bb8f72fa44f69e27a383f05d0
Num Joseph <konum5437@gmail.com>
2023-11-30 06:29:06 -0800

    Second commit

---
+++ prisma/migrations/20231129055710_joe/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Voter" (
+    "id" SERIAL NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Voter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Voter_email_key" ON "Voter"("email");
--- server.js
+++ server.js
@@ -6,8 +6,24 @@ const prisma = new PrismaClient();
 
 const PORT = 3030;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome to our campus voting App!");
+});
+
+app.post("/signup", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    const newVoter = await prisma.voter.create({
+      data: { fullName, email, password },
+    });
+
+    res.status(200).json({ newVoter });
+  } catch (error) {
+    console.log(error);
+    res.status(409).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(PORT, () => {
